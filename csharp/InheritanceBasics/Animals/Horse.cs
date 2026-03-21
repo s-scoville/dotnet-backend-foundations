93ab4d30959e71ba0a31f8a5295d909b88e48861
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-21 07:36:17 -0500

    feat: create Horse class with constructor that inherits from Animal

--- csharp/InheritanceBasics/Animals/Horse.cs
+++ csharp/InheritanceBasics/Animals/Horse.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace InheritanceBasics.Animals
 {
-    public class Horse
+    public class Horse : Animal
     {
-
+        public bool HasShoes { get; private set; }
+        public Horse(string name, int age, double weight, string breed, bool hasShoes):base(name, age, weight, breed)
+        {
+            HasShoes = hasShoes;
+        }
     }
 }

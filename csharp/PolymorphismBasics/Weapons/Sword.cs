d97b25a47af26de7a172742d9d79f70dd9e8ac65
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-22 10:35:06 -0500

    feat: add Sword class inheriting from Weapon

---
+++ csharp/PolymorphismBasics/Weapons/Sword.cs
@@ -0,0 +1,10 @@
+﻿namespace PolymorphismBasics.Weapons
+{
+    public class Sword : Weapon
+    {
+        public Sword(string name, double weight, string material, int baseDamage) : base(name, weight, material, baseDamage)
+        {
+
+        }
+    }
+}

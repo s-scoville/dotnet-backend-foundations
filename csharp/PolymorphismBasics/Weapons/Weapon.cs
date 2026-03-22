0fca2e66778aa4af386c1b8459ec7addd4d87d32
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-22 10:27:22 -0500

    feat: add abstract Weapon base class with constructor and shared properties

---
+++ csharp/PolymorphismBasics/Weapons/Weapon.cs
@@ -0,0 +1,18 @@
+﻿namespace PolymorphismBasics.Weapons
+{
+    public abstract class Weapon
+    {
+        public string Name { get; }
+        public double Weight { get; }
+        public string Material { get; }
+        public int BaseDamage { get; }
+
+        public Weapon(string name, double weight, string material, int baseDamage)
+        {
+            Name = name;
+            Weight = weight;
+            Material = material;
+            BaseDamage = baseDamage;
+        }
+    }
+}

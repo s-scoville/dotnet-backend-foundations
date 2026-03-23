64cbf989492cafcfae5a88cbe447828f69b14b20
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-23 09:01:44 -0500

    feat: add Bow class with piercing damage and Attack implementation

---
+++ csharp/PolymorphismBasics/Weapons/Bow.cs
@@ -0,0 +1,17 @@
+﻿namespace PolymorphismBasics.Weapons
+{
+    public class Bow : Weapon
+    {
+        public int PiercingDamage { get; }
+
+        public Bow(string name, double weight, string material, int baseDamage, int piercingDamage): base(name, weight, material, baseDamage)
+        {
+            PiercingDamage = piercingDamage;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"You shoot the enemy for {BaseDamage + PiercingDamage} damage.");
+        }
+    }
+}

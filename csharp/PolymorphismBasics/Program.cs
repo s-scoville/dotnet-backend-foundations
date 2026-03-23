9509f9be8103c7f775d847148cc5651770cf92c1
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-23 09:09:05 -0500

    feat: implement polymorphic weapon system with Attack behavior

--- csharp/PolymorphismBasics/Program.cs
+++ csharp/PolymorphismBasics/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PolymorphismBasics.Weapons;
+
+List<Weapon> weapons = new List<Weapon>();
+weapons.Add(new Sword("Copper Sword", 10, "Copper", 5, 5));
+weapons.Add(new Bow("Wooden Bow", 6, "Wood", 5, 10));
+
+foreach (Weapon w in weapons)
+{
+    w.Attack();
+}

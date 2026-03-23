044fdda3a940944d3acc6bcb66de89ef22d7745e
Steven Scoville <s.scoville.dev@outlook.com>
2026-03-23 09:19:03 -0500

    feat: add Mace class and integrate into polymorphic weapon system

--- csharp/PolymorphismBasics/Program.cs
+++ csharp/PolymorphismBasics/Program.cs
@@ -3,6 +3,7 @@
 List<Weapon> weapons = new List<Weapon>();
 weapons.Add(new Sword("Copper Sword", 10, "Copper", 5, 5));
 weapons.Add(new Bow("Wooden Bow", 6, "Wood", 5, 10));
+weapons.Add(new Mace("Iron Mace", 15, "Iron", 10, 20));
 
 foreach (Weapon w in weapons)
 {
---
+++ csharp/PolymorphismBasics/Weapons/Mace.cs
@@ -0,0 +1,17 @@
+﻿namespace PolymorphismBasics.Weapons
+{
+    public class Mace : Weapon
+    {
+        public int BluntDamage { get; }
+
+        public Mace(string name, double weight, string material, int baseDamage, int bluntDamage): base(name, weight, material, baseDamage)
+        {
+            BluntDamage = bluntDamage;
+        }
+
+        public override void Attack()
+        {
+            Console.WriteLine($"You crush the enemy for {BaseDamage + BluntDamage} damage.");
+        }
+    }
+}

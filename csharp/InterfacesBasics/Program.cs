423e9f58e2cc04c0c1a3d9fe7533c18d4dda7b62
Steven Scoville <s.scoville.dev@outlook.com>
2026-04-20 07:54:55 -0500

    feat: implement notification system using INotification interface

--- csharp/InterfacesBasics/Program.cs
+++ csharp/InterfacesBasics/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using InterfacesBasics.Notifications;
+
+List<INotification> notifications = new List<INotification>();
+notifications.Add(new EmailNotification());
+notifications.Add(new TextNotification());
+notifications.Add(new PushNotification());
+
+foreach (INotification n in notifications)
+{
+    n.Send();
+    Console.WriteLine();
+}

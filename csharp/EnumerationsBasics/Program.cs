fbc50b2dd6767e67cedb3b7cb91f0ce7ce835c36
Steven Scoville <s.scoville.dev@outlook.com>
2026-06-19 13:40:44 -0500

    feat: test ClassType enum in Program.cs using ShowClassMessage

--- csharp/EnumerationsBasics/Program.cs
+++ csharp/EnumerationsBasics/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EnumerationsBasics.Models;
+using EnumerationsBasics.Services;
+
+var showClassMessage = new ShowClassMessage();
+showClassMessage.Display(ClassType.Paladin);
+showClassMessage.Display(ClassType.Mage);
+showClassMessage.Display(ClassType.Rogue);

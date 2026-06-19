c7ca1b1dbc1eee63adfd9c1f481ec51e0a26b6f9
Steven Scoville <s.scoville.dev@outlook.com>
2026-06-19 13:30:10 -0500

    feat: add ShowClassMessage service with Display method

---
+++ csharp/EnumerationsBasics/Services/ShowClassMessage.cs
@@ -0,0 +1,32 @@
+﻿using EnumerationsBasics.Models;
+
+namespace EnumerationsBasics.Services
+{
+    public class ShowClassMessage
+    {
+        public void Display(ClassType classType)
+        {
+            switch (classType)
+            {
+                case ClassType.Warrior:
+                    Console.WriteLine("You are a mighty Warrior!");
+                    break;
+                case ClassType.Mage:
+                    Console.WriteLine("You are a powerful Mage!");
+                    break;
+                case ClassType.Rogue:
+                    Console.WriteLine("You are a stealthy Rogue!");
+                    break;
+                case ClassType.Hunter:
+                    Console.WriteLine("You are a skilled Hunter!");
+                    break;
+                case ClassType.Paladin:
+                    Console.WriteLine("You are a noble Paladin!");
+                    break;
+                default:
+                    Console.WriteLine("Unknown class type!");
+                    break;
+            }
+        }
+    }
+}
